91414d1f5ecc62e89875575f4bb88b507aa7acf3
Donatas Dautartas <donatasd@wix.com>
2017-02-23 10:01:08 +0200

    unordered list

--- src/RichTextArea/RichTextArea.js
+++ src/RichTextArea/RichTextArea.js
@@ -3,7 +3,7 @@ import {Editor} from 'slate';
 import WixComponent from '../WixComponent';
 import RichTextEditorToolbar from './RichTextAreaToolbar';
 import htmlSerializer from './htmlSerializer';
-
+const DEFAULT_NODE = 'paragraph'
 class RichTextArea extends WixComponent {
   /* eslint-disable react/prop-types */
   schema = {
@@ -42,6 +42,23 @@ class RichTextArea extends WixComponent {
     onChange && onChange(htmlSerializer.serialize(this.state.editorState));
   }
 
+  hasBlock = type => {
+    const { editorState } = this.state;
+    return editorState.blocks.some(node => node.type == type);
+  };
+
+  handleButtonClick = (action, type) => {
+    switch (action) {
+      case 'mark':
+        return this.handleMarkButtonClick(type);
+      case 'block':
+        return this.handleBlockButtonClick(type);
+      case 'link':
+        // @TODO: implement
+        return;
+    }
+  };
+
   handleMarkButtonClick = type => {
     const editorState = this.state.editorState
       .transform()
@@ -51,10 +68,60 @@ class RichTextArea extends WixComponent {
     this.setEditorState(editorState);
   };
 
+  handleBlockButtonClick = type => {
+    let { editorState } = this.state;
+    let transform = editorState.transform();
+    const { document } = editorState;
+
+    // Handle everything but list buttons.
+    if (type !== 'unordered-list' && type !== 'ordered-list') {
+      const isActive = this.hasBlock(type);
+      const isList = this.hasBlock('list-item');
+
+      if (isList) {
+        transform
+          .setBlock(isActive ? '' : type)
+          .unwrapBlock('unordered-list')
+          .unwrapBlock('ordered-list');
+      }
+
+      else {
+        transform
+          .setBlock(isActive ? '' : type);
+      }
+    }
+
+    // Handle the extra wrapping required for list buttons.
+    else {
+      const isList = this.hasBlock('list-item');
+      const isType = editorState.blocks.some((block) => {
+        return !!document.getClosest(block.key, parent => parent.type == type);
+      });
+
+      if (isList && isType) {
+        transform
+          .setBlock(DEFAULT_NODE)
+          .unwrapBlock('unordered-list')
+          .unwrapBlock('ordered-list');
+      } else if (isList) {
+        transform
+          .unwrapBlock(type == 'unordered-list' ? 'ordered-list' : 'unordered-list')
+          .wrapBlock(type);
+      } else {
+        transform
+          .setBlock('list-item')
+          .wrapBlock(type);
+      }
+    }
+
+    editorState = transform.apply();
+    this.setState({editorState});
+  };
+
   render = () => {
     return (
       <div>
-        <RichTextEditorToolbar onClick={this.handleMarkButtonClick}/>
+        <RichTextEditorToolbar onClick={this.handleButtonClick}/>
         <Editor
           schema={this.schema}
           state={this.state.editorState}
--- src/RichTextArea/RichTextAreaToolbar.js
+++ src/RichTextArea/RichTextAreaToolbar.js
@@ -2,21 +2,21 @@ import React, {PropTypes} from 'react';
 import WixComponent from '../WixComponent';
 
 class RichTextAreaToolbar extends WixComponent {
-  getMouseDownHandler(type) {
+  getMouseDownHandler(action, type) {
     return event => {
       const {onClick} = this.props;
       event.preventDefault();
-      onClick && onClick(type);
+      onClick && onClick(action, type);
     };
   }
 
   render() {
     return (
       <div>
-        <button data-hook="rich-text-area-button-bold" onMouseDown={this.getMouseDownHandler('bold')}>B</button>
-        <button data-hook="rich-text-area-button-italic" onMouseDown={this.getMouseDownHandler('italic')}>I</button>
-        <button data-hook="rich-text-area-button-underline" onMouseDown={this.getMouseDownHandler('underline')}>U</button>
-        <button data-hook="rich-text-area-button-unordered-list" onMouseDown={this.getMouseDownHandler('unordered-list')}>UL</button>
+        <button data-hook="rich-text-area-button-bold" onMouseDown={this.getMouseDownHandler('mark', 'bold')}>B</button>
+        <button data-hook="rich-text-area-button-italic" onMouseDown={this.getMouseDownHandler('mark', 'italic')}>I</button>
+        <button data-hook="rich-text-area-button-underline" onMouseDown={this.getMouseDownHandler('mark', 'underline')}>U</button>
+        <button data-hook="rich-text-area-button-unordered-list" onMouseDown={this.getMouseDownHandler('block', 'unordered-list')}>UL</button>
       </div>
     );
   }
