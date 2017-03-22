abd532741cc89d2eb1f63c87254c10282d530880
Boris Dahtiar <borisd@wix.com>
2017-03-22 18:51:46 +0200

    fix build for e2e-sandbox

--- testkit/e2e-sandbox.js
+++ testkit/e2e-sandbox.js
@@ -3,19 +3,16 @@ const app = express();
 
 if (process.argv[2] === undefined) {
   console.log('provide web server port !');
-  return;
-}
-
-if (process.argv[3] === undefined) {
+} else if (process.argv[3] === undefined) {
   console.log('provide static file directory !');
-  return;
+} else {
+
+  const PORT = process.env.PORT || process.argv[2];
+  const staticFiles = process.argv[3];
+
+  app.use('/', express.static(staticFiles));
+
+  app.listen(PORT, () => {
+    console.log('e2e sandbox is running on port %s, serving %s', PORT, staticFiles);
+  });
 }
-
-const PORT = process.env.PORT || process.argv[2];
-const staticFiles = process.argv[3];
-
-app.use('/', express.static(staticFiles));
-
-app.listen(PORT, () => {
-  console.log('e2e sandbox is running on port %s, serving %s', PORT, staticFiles);
-});
